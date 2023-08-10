4822251e9ef2b1348389158f71a6bf8c7b4210be
HuangWei <market@zuiyue.com>
2023-08-10 16:56:35 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use systray::Application;
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = Application::new().unwrap();
+    let home_dir = wei_env::home_dir().unwrap();
+    let ico_path = std::path::Path::new(&home_dir).join("AppData\\Local\\Ai\\res\\ai.ico");
+
+    app.set_icon_from_file(&ico_path.to_string_lossy()).unwrap();
+    app.add_menu_item(&"打开主界面".to_string(), |_| {
+        let _ = webbrowser::open("https://www.zuiyue.com");
+        Ok::<_, systray::Error>(())
+    }).unwrap();
+    app.add_menu_item(&"退出".to_string(), |window| {
+        window.quit();
+        Ok::<_, systray::Error>(())
+    }).unwrap();
+    app.wait_for_message().unwrap();
 }
